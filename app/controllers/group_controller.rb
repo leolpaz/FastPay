858f5da8617687f686c3e9ee648251ebeb9b846d
leolpaz <leonardolpaz95@gmail.com>
2022-02-07 16:35:35 -0300

    Setup cancancan

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -1,2 +1,3 @@
 class GroupController < ApplicationController
+  load_and_authorize_resource
 end
--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -1,2 +1,3 @@
 class PaymentController < ApplicationController
+  load_and_authorize_resource
 end
