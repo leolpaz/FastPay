222e7955e718cfe87ce638d7e337f2baf2c60f06
leolpaz <leonardolpaz95@gmail.com>
2022-02-08 21:00:18 -0300

    Fix issues pointed by style lint and rubocop

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,9 @@
 # Load the Rails application.
-require_relative "application"
+require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+ActionView::Base.field_error_proc = proc do |html_tag, _instance|
   html_tag.html_safe
 end
