1e0261bd941ea6668b150940c1d68817c3d9fe23
leolpaz <leonardolpaz95@gmail.com>
2022-02-10 13:36:27 -0300

    Create tests for group model

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) {
+    @u = User.new(name: 'test', password: 'testpass', email: 'test@test.com')
+    @u.save
+    @g = @u.groups.new(name: 'test', icon: 'food') 
+  }
+  it 'name should be present' do
+    expect(@g).to be_valid
+    @g.name = nil
+    expect(@g).to_not be_valid
+  end
+  it 'icon should be present' do
+    expect(@g).to be_valid
+    @g.icon = nil
+    expect(@g).to_not be_valid
+  end
+  it 'should belong to an user' do
+    expect(@g).to be_valid
+    @g = Group.new(name: 'test', icon: 'food') 
+    expect(@g).to_not be_valid
+  end
 end
